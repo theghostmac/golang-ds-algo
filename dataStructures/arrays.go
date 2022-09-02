1e4e616fa5edbe0b472c02b5df802d4d6af754e3
MacBobby Chibuzor <theghostmac@gmail.com>
2022-09-02 23:31:57 +0100

    about arr

--- dataStructures/arrays.go
+++ dataStructures/arrays.go
@@ -1,1 +1,8 @@
 package dataStructures
+
+import "fmt"
+
+func main() {
+	var arr = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(arr)
+}
--- dataStructures/tests/create_array_test.go
+++ dataStructures/tests/create_array_test.go
@@ -1,32 +1,32 @@
 package tests
 
-import (
-	"testing"
-
-	"github.com/theghostmac/golang-ds-algo/dataStructures"
-)
-
-func TestGetFunction(t *testing.T) {
-	arr := dataStructures.Array{}
-
-	tests := []struct{
-		expected interface{}
-	}{
-		{1},
-		{"1"},
-	}
-	arr.Push(1)
-	arr.Push("1")
-
-	for i, tst := range tests {
-		value, err := arr.Get(i)
-			if err != nil {
-				t.Fatalf("Index %d is out of range. Array length is %d", i, arr.Length())
-			}
-		}
-
-		if value != tst.expected {
-			t.Fatalf("wrong value. Expected: %v, got %v", tst.expected, value)
-		}
-	}
-}
+//import (
+//	"testing"
+//
+//	"github.com/theghostmac/golang-ds-algo/dataStructures"
+//)
+//
+//func TestGetFunction(t *testing.T) {
+//	arr := dataStructures.Array{}
+//
+//	tests := []struct{
+//		expected interface{}
+//	}{
+//		{1},
+//		{"1"},
+//	}
+//	arr.Push(1)
+//	arr.Push("1")
+//
+//	for i, tst := range tests {
+//		value, err := arr.Get(i)
+//			if err != nil {
+//				t.Fatalf("Index %d is out of range. Array length is %d", i, arr.Length())
+//			}
+//		}
+//
+//		if value != tst.expected {
+//			t.Fatalf("wrong value. Expected: %v, got %v", tst.expected, value)
+//		}
+//	}
+//}
