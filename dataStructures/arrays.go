56d49903680627db9805a05fef463a11bd511e96
MacBobby Chibuzor <theghostmac@gmail.com>
2022-09-02 23:34:54 +0100

    accessing and ranging arr

--- dataStructures/arrays.go
+++ dataStructures/arrays.go
@@ -5,4 +5,13 @@ import "fmt"
 func main() {
 	var arr = [5]int{1, 2, 3, 4, 5}
 	fmt.Println(arr)
+	// access all elements
+	var i int
+	for i = 0; i < len(arr); i++ {
+		fmt.Println("Printing array elements: ", arr[i])
+	}
+	var value int
+	for i, value = range arr {
+		fmt.Println(" range ", value)
+	}
 }
